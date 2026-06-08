5fc729f0bc58dee4e217ce2a1bf8ff4d17390d03
n5ad <n5ad@att.net>
2026-06-08 14:59:05 -0500

    Create list_mp3.php

---
+++ list_mp3.php
@@ -0,0 +1,15 @@
+<?php
+/** list_mp3.php - HamvoIP version */
+$files = [];
+$mp3_files = glob("/mp3/*.mp3");
+foreach ($mp3_files as $f) $files[] = basename($f);
+
+$wav_files = glob("/mp3/*.wav");
+foreach ($wav_files as $f) $files[] = basename($f);
+
+sort($files);
+$files = array_unique($files);
+
+header('Content-Type: application/json');
+echo json_encode($files);
+?>
