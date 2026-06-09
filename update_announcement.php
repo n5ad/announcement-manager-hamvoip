7d41b6b26aefa8bc0f8e097e8b8b2c5ec58bb82d
n5ad <n5ad@att.net>
2026-06-09 13:47:00 -0500

    Update update_announcement.php

--- update_announcement.php
+++ update_announcement.php
@@ -1,3 +1,6 @@
+// update_announcement.php
+// Modified from Announcement Manager for ASL3 created by James N5AD June 2026
+
 <?php
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405); echo "Method not allowed."; exit;
