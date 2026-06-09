81324120067c20dd7d48511f7c8be6b3d2f2c221
n5ad <n5ad@att.net>
2026-06-08 20:47:51 -0500

    Update run_announcement.php

--- run_announcement.php
+++ run_announcement.php
@@ -1,7 +1,9 @@
 <?php
-// run_announcement.php - HamvoIP version
+// run_announcement.php - Fixed for HamvoIP
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405); echo "Method not allowed."; exit;
+    http_response_code(405); 
+    echo "Method not allowed."; 
+    exit;
 }
 
 $base = basename($_POST['file'] ?? '');
@@ -9,10 +11,9 @@ $source = $_POST['source'] ?? 'ul';
 
 if ($source === 'mp3') {
     $play_path = "/mp3/" . pathinfo($base, PATHINFO_FILENAME);
-    $echo_msg = "Playing '$base' (MP3/WAV) locally.";
 } else {
-    $play_path = "announcements/" . pathinfo($base, PATHINFO_FILENAME);
-    $echo_msg = "Playing '$base' (.ul) locally.";
+    // Use full path for .ul files
+    $play_path = "/usr/local/share/asterisk/sounds/announcements/" . pathinfo($base, PATHINFO_FILENAME);
 }
 
 $play_script = "/etc/asterisk/local/playaudio.sh";
@@ -26,8 +27,8 @@ $cmd = escapeshellcmd("sudo $play_script " . escapeshellarg($play_path));
 exec($cmd . " 2>&1", $output, $retval);
 
 if ($retval === 0) {
-    echo $echo_msg;
+    echo "Local playback started for '$base'.";
 } else {
-    echo "Failed to play. Code: $retval";
+    echo "Local play failed.\nCode: $retval\n" . implode("\n", $output);
 }
 ?>
